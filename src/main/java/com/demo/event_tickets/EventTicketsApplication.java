8614f66779f5132ef6c97c3e893406f795bd215c
Lohansa Munasinghe <lohansamunasinghe3@gmail.com>
2024-12-08 18:35:02 +0530

    Initial commit

---
+++ src/main/java/com/demo/event_tickets/EventTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.demo.event_tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/event_tickets/EventTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.event_tickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
